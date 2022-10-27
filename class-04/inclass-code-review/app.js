f47c8835c7087df3c1c44eb95c4733340a1e12b9
reyjroliva <reyjroliva@ufl.edu>
2022-10-27 01:22:04 -0700

    added skeleton for code review

---
+++ class-04/inclass-code-review/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+console.log('is this thing on?');
+
+// Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
+// Indicates through an alert if the guess is “too high” or “too low”.
+// It should give the user exactly four opportunities to get the correct answer. Consider using a loop of some sort.
+// After all attempts have been exhausted, tell the user the correct answer.
+
+
+
+
+
+
+
+
+
+// Add a 7th question that has multiple possible correct answers that are stored in an array.
+// Give the user 6 attempts to guess the correct answer.
+// The guesses will end once the user guesses a correct answer or they run out of attempts.
+// Display all the possible correct answers to the user.
+// Consider using a loop of some sort for this question.
